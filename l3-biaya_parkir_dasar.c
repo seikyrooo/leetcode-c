07e58dfc917f25915ed43491177019b890129f2c
Dwiko Dany Rananta <dwiko.dev@gmail.com>
2025-09-06 18:45:51 +0700

    koreksi level 3

--- l3-biaya_parkir_dasar.c
+++ l3-biaya_parkir_dasar.c
@@ -5,19 +5,22 @@
 // print biaya
 
 #include <stdio.h>
-#include <time.h>
 
 int main() {
-    int masuk, keluar, durasi;
-    long biaya;
+    int masuk, keluar;
     printf("Masukan jam masuk dan keluar dengan format int Contoh (8 10) : ");
-    scanf("%d %d", &masuk, &keluar);
-    durasi = keluar - masuk;
-    if (durasi <= 2) {
-        biaya = 3000;
-    }else {
-        biaya = 3000 + (durasi - 2) * 1000;
-    }
-    printf("Biaya Parkir anda adalah : Rp. %ld", biaya);
+    if (scanf("%d %d", &masuk, &keluar) != 2) {puts("Input tidak valid"); return 1; }
+
+    if (keluar < masuk) {puts("Jam Keluar harus lebih kecil daripada jam masuk"); return 1;}
+
+    int durasi = keluar - masuk;
+
+    int biaya = (durasi <= 2) ? 3000 : 3000 + (durasi - 2 ) * 1000;
+    // if (durasi <= 2) {
+    //     biaya = 3000;
+    // }else {
+    //     biaya = 3000 + (durasi - 2) * 1000;
+    // }
+    printf("Biaya Parkir anda adalah : Rp. %d\n", biaya);
     return 0;
 }
